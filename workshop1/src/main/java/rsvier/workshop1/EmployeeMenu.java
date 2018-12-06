7b06b1f8eb3db7b092f73ee1cf731a96a35a38e5
Imke <Imke@192.168.1.12>
2018-12-06 10:58:25 +0100

    Commit Peter

--- workshop1/src/main/java/rsvier/workshop1/EmployeeMenu.java
+++ workshop1/src/main/java/rsvier/workshop1/EmployeeMenu.java
@@ -9,15 +9,15 @@ public class EmployeeMenu {
 			+ "1 = Customers \n"
 			+ "2 = Orders \n"
 			+ "3 = Product catalog \n"
-			+ "9 = Logout");
+			+ "9 = System.exit(0)");
 
 	Scanner input = new Scanner(System.in);
     
     int inputOption = input.nextInt();
 	switch (inputOption) {
 	case 1 : new CustomerMenu(); break;
-	case 2 : new OrderMenu();
-	case 3 : new ProductCatalog();
+	case 2 : new OrderMenu(); break;
+	case 3 : new ProductCatalog(); break;
 	case 9 : new EmployeeMenu(); break;
 	default : System.out.println("Please choose 1,2,3 or 9"); break;
 	
