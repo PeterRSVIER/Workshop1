142ee56edb2f472425c32c6a9274bec2a3a85f64
Imke <Imke@192.168.1.12>
2018-12-06 10:16:48 +0100

    Commit Peter

--- workshop1/src/main/java/rsvier/workshop1/EmployeeMenu.java
+++ workshop1/src/main/java/rsvier/workshop1/EmployeeMenu.java
@@ -16,11 +16,13 @@ public class EmployeeMenu {
     int inputOption = input.nextInt();
 	switch (inputOption) {
 	case 1 : CustomerMenu customer = new CustomerMenu(); break;
-//	case 2 : OrderMenu order = new OrderMenu();
+	case 2 : OrderMenu orderMenu = new OrderMenu();
 	case 9 : EmployeeMenu employee = new EmployeeMenu(); break;
-	default : System.out.println("Please choose 1,2,3 or 9"); break;
-	
-	}
-	
+	default : System.out.println("Please choose 1,2,3 or 9"); break;}
    }
+	public void Menu2(){
+	System.out.println("menu2");
+	}
+
+   
 }
