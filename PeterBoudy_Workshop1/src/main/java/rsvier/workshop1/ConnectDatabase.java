b5e84c8175a49413a59775092f2ae99c02dfa563
Peter de Graaf <peterdegraaf1991@hotmail.com>
2018-12-11 16:07:24 +0100

    Peter

--- PeterBoudy_Workshop1/src/main/java/rsvier/workshop1/ConnectDatabase.java
+++ PeterBoudy_Workshop1/src/main/java/rsvier/workshop1/ConnectDatabase.java
@@ -7,20 +7,10 @@ import java.sql.Statement;
 
 public class ConnectDatabase {
 	public static void main(String[] args ) throws SQLException, ClassNotFoundException{
-	try {
-		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver ());
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	try {	
+
 
 	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/pb_workshop1", "root", "rsvier");
 	Statement statement = connection.createStatement();
-	}
-	catch (SQLException ex) {
-		System.out.println(ex.toString());
-	}
 	System.out.println("Connection Succesfull");
 }
 }
