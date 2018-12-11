0eac2d2f92eb6eea1547b068ec963efb4d90958d
Peter de Graaf <peterdegraaf1991@hotmail.com>
2018-12-11 14:24:37 +0100

    Peter

---
+++ PeterBoudy_Workshop1/src/main/java/rsvier/workshop1/OrderLine.java
@@ -0,0 +1,56 @@
+package rsvier.workshop1;
+
+public class OrderLine {
+
+	private final int orderId
+	,       productId;
+	private int amount;
+
+	public OrderLine(int orderId, int productId, int amount) {
+		this.orderId = orderId;
+		this.productId = productId;
+		this.amount = amount;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + amount;
+		result = prime * result + productId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderLine other = (OrderLine) obj;
+		if (amount != other.amount)
+			return false;
+		if (productId != other.productId)
+			return false;
+		return true;
+	}
+	
+}
---
+++ PeterBoudy_Workshop1/src/main/java/rsvier/workshop1/Product.java
@@ -0,0 +1,73 @@
+package rsvier.workshop1;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+	private String productName;
+	private int productId
+	,           stock;//#of this product in stock
+	private BigDecimal price;  //instead of BigNumber
+
+    private Product(String productName, int productId, BigDecimal price, int stock) {
+      this.productName = productName;
+      this.productId = productId;
+      this.price = price;
+      this.stock = stock;
+    }
+
+	public String getProductName() {
+		return productName;
+		}
+
+	public int getProductId() {
+		return productId;
+		}
+
+	public BigDecimal getPrice() {
+		return price;
+		}
+
+	public int getStock() {
+		return stock;
+		}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+		}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+		}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+		}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (productName == null) {
+			if (other.productName != null)
+				return false;
+		} else if (!productName.equals(other.productName))
+			return false;
+		return true;
+	}
+
+}
+
