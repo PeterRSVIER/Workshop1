66dcf98c578b2753f1d45c03c6245df65983d9fc
Peter de Graaf <peterdegraaf1991@hotmail.com>
2018-12-17 12:24:58 +0100

    Logger

--- PeterBoudy_Workshop1/src/main/java/connection/login.java
+++ PeterBoudy_Workshop1/src/main/java/connection/login.java
@@ -5,30 +5,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
-
-import utility.LogConfig;
-
 
 public class login {
 	public static void main(String[] args ) throws ClassNotFoundException, SQLException{
 		
-		final Logger logger = LogConfig.getLogger();
-//		Specify LogLevel for Class here, default = ALL
-		LogConfig.setHandlerLevel(Level.ALL);
-//
-		
+final Logger LOG = LoggerFactory.getLogger(login.class);
+
 		String username = null;
 		String password = null;
 		String url = null;
 		
 			    try {
 				File xmlFile = new File("src/main/java/connection/LoginDetails.xml");
+				if (xmlFile.exists()) {
 				DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 				DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
 				Document document = documentBuilder.parse(xmlFile);
@@ -38,25 +34,28 @@ public class login {
 				password = document.getElementsByTagName("password").item(0).getTextContent();
 				url = document.getElementsByTagName("url").item(0).getTextContent();
 			    } 
-			    
-			    //Which Exceptions do we need to catch? (by Peter)
+				else {
+				LOG.info("xmlFile not found");
+				}
+				}
+			    //Which Exceptions do we need to catch? (by Peter) bijv. bestand bestaat niet.
 			    catch (Exception ex) {
 				ex.printStackTrace();
 			    }
-			    
-				finally {
-				    System.out.println("username:" + username);
-				    System.out.println("password:" + password);
-				}
-    logger.entering("Trying to login to database with username:",username);//
-    logger.entering("and password:", password);//
-    logger.entering("url:",url);
-    
-	Connection connection = DriverManager.getConnection(url, username, password);
 
-	logger.log(Level.INFO, "Succesfully connected to Database as: " + username);
+	LOG.info("Trying to login to database with: \n"
+			+ "Username: "+ username + "\n"
+			+ "Password " + password + "\n"
+			+ "URL: " + url + "\n");
 
-	Statement statement = connection.createStatement();
+    try {
+	Connection connection = DriverManager.getConnection(url,username,password);
+    }
+    catch (Exception ex) {
+    ex.printStackTrace();
+	}
+	
+    LOG.info("Succesfully connected to Database as: " + username);
 
 	}
 }
